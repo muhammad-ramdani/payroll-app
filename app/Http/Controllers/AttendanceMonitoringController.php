39175f3eec0ec4d4787129fb423fd336980144b9
Muhammad Ramdani <muhammadramdanibjrs@gmail.com>
2025-05-24 18:36:39 +0700

    feat: Remove unused Request import from AttendanceMonitoringController

--- app/Http/Controllers/AttendanceMonitoringController.php
+++ app/Http/Controllers/AttendanceMonitoringController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Attendance;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class AttendanceMonitoringController extends Controller
