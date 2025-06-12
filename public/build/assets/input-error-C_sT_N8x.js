a70812031df9f2b55d19c22db8764fe093afc99f
Muhammad Ramdani <muhammadramdanibjrs@gmail.com>
2025-06-12 20:21:05 +0700

    Remove unused JavaScript files for table and textarea components, and delete the manifest.json file to clean up the build assets.

--- public/build/assets/input-error-C_sT_N8x.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-yxEi3xJe.js";import{a as o}from"./app-logo-icon-CXNx6eOd.js";function a({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{a as I};
